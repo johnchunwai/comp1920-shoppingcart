01e44e8df6640e2f398621b2ffe482a0e0c489de
johnchunwai@gmail.com <johnchunwai@gmail.com@c7039aa2-1800-fe5e-8d3f-ba386e00764b>
2011-11-27 11:39:29 +0000

    Fix the SQL script and added a few images for the products.

--- trunk/database_shoppingcart.sql
+++ trunk/database_shoppingcart.sql
@@ -9,6 +9,7 @@ USE shoppingcart;
 DROP TABLE IF EXISTS tbl_order_item;
 DROP TABLE IF EXISTS tbl_order;
 DROP TABLE IF EXISTS tbl_inventory;
+DROP TABLE IF EXISTS tbl_product;
 DROP TABLE IF EXISTS tbl_customer;
 
 
@@ -22,8 +23,8 @@ CREATE TABLE IF NOT EXISTS tbl_customer(
 
 CREATE TABLE IF NOT EXISTS tbl_product(
 	prod_id int not null primary key auto_increment,
-	prod_name varchar(20) not null,
-	prod_desc varchar(200) not null,
+	prod_name varchar(100) not null,
+	prod_desc varchar(1000) not null,
 	prod_image varchar(500) not null 
 ) ENGINE=InnoDB;
 
@@ -51,3 +52,18 @@ CREATE TABLE IF NOT EXISTS tbl_order_item(
     CONSTRAINT fk_tbl_order_item_tbl_order_ord_id FOREIGN KEY (ord_id) REFERENCES tbl_order (ord_id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT fk_tbl_order_item_tbl_product_prod_id FOREIGN KEY (prod_id) REFERENCES tbl_product (prod_id) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
+
+/*
+	Test Data
+*/
+INSERT INTO tbl_customer (cus_name, cus_loginname, cus_password, cus_email) VALUES
+	('Chun Wai Chan', 'cchan', 'comp1920', 'cchan331@my.bcit.ca'),
+	('Bill Yu', 'byu', 'comp1920', 'byu39@my.bcit.ca'),
+	('Patrick Kowalski', 'pkowalski', 'comp1920', 'pkowalski@my.bcit.ca'),
+	('Sahra Dilmaghanyan', 'sdilmaghanyan', 'comp1920', 'sdilmaghanyan@my.bcit.ca'),
+	('Sami Jashromi', 'sjahromi', 'comp1920', 'seshraghjahromi@my.bcit.ca');
+
+INSERT INTO tbl_product (prod_name, prod_desc, prod_image) VALUES
+	('Radioactive Blue Cheese', 'Accidental by-product of an experiemental drug.', './img/radioactive_blue_inv.gif'),
+	('Dry Cow Dung', 'Quite a lot of skill needed to be applied while making these piles.', './img/dry_cow_dung.jpg'),
+	('Monopoly Bill', 'A monopoly bill where you can write any amount onto it.', './img/bill.jpg');
