9f203b8152d5e8b41145e42f62e8fe48cc6d3e62
johnchunwai@gmail.com <johnchunwai@gmail.com@c7039aa2-1800-fe5e-8d3f-ba386e00764b>
2011-12-02 10:05:22 +0000

    Created tag 1.00.02. Fix use of $_REQUEST by removing $_COOKIE from it.

---
+++ tags/1.00.02/login.php
@@ -0,0 +1,104 @@
+<?php
+	/* Author: Bill Yu, Chun Wai Chan, Patrick Kowalski, Sahra Dilmaghanyan, Sami Jashromi
+	 * Date: Nov 17, 2011
+	 * File name: login.php
+	 * File description: Handles login and logout of users.
+	 */
+	require_once('common.php');
+		
+	/*
+	 * Authenticates user. Returns succeed/failure, error_msg. eg. { "succeed":true } or { "succeed":false, "msg":"error message" }.
+	 */
+	function login($loginname, $password) {
+		$succeed = false;
+		$msg = "";
+		setcookie('loginname', $loginname, time() + 3600);
+		if (USE_DB) {
+			$loginname = mysql_real_escape_string($loginname);
+			$password = mysql_real_escape_string($password);
+			$q = "SELECT cus_id, cus_name FROM tbl_customer WHERE cus_loginname = '$loginname' AND cus_password = '$password'";
+			if ($rs = mysql_query($q)) {
+				if ($r = mysql_fetch_assoc($rs)) {
+					$succeed = true;
+					$_SESSION['login'] = true;
+					$_SESSION['loginname'] = $loginname;
+					$_SESSION['cus_name'] = $r['cus_name'];
+					$_SESSION['cus_id'] = $r['cus_id'];
+				}
+				else {
+					$msg = "Invalid credential.";
+				}
+			}
+			else {
+				$msg = mysql_error();
+			}
+		}
+		else {
+			if ($loginname == "comp1920" && $password == "comp1920") {
+				$succeed = true;
+				$_SESSION['login'] = true;
+				$_SESSION['loginname'] = $loginname;
+				$_SESSION['cus_name'] = 'John Chan';
+				$_SESSION['cus_id'] = 1;
+			}
+		}
+		return array('succeed' => $succeed, 'msg' => $msg);
+	}
+	
+	/*
+	 * Returns the user login info. eg. { "login":true, "loginname":"johnchan" "cus_name": "John Chan"} or { "login":false }.
+	 */
+	function getLoginInfo() {
+		if (validateSession()) {
+			$result['login'] = $_SESSION['login'];
+			$result['loginname'] = $_SESSION['loginname'];
+			$result['cus_name'] = $_SESSION['cus_name'];
+		}
+		else {
+			$result['login'] = false;
+			if (empty($_REQUEST['loginname']) && isset($_COOKIE['loginname'])) {
+				$result['loginname'] = $_COOKIE['loginname'];
+			}
+			else {
+				$result['loginname'] = null;
+			}
+			$result['cus_name'] = null;
+		}
+		return $result;
+	}
+	
+	/*
+	 * Logout the current user.
+	 */
+	function logout() {
+		// Unset all of the session variables.
+		$_SESSION = array();
+		
+		// If it's desired to kill the session, also delete the session cookie.
+		// Note: This will destroy the session, and not just the session data!
+		if (ini_get("session.use_cookies")) {
+		    $params = session_get_cookie_params();
+		    setcookie(session_name(), '', time() - 42000,
+		        $params["path"], $params["domain"],
+		        $params["secure"], $params["httponly"]
+		    );
+		}
+		
+		// Finally, destroy the session.
+		session_destroy();
+	}
+	
+	/*
+	 * Script to invoke methods.
+	 */
+	$method = $_REQUEST['method'];
+	if ($method == 'login') {
+		echo json_encode(login($_REQUEST['loginname'], $_REQUEST['password']));
+	}
+	else if ($method == 'getLoginInfo') {
+		echo json_encode(getLoginInfo());
+	}
+	else if ($method == 'logout') {
+		logout();
+	}
+?>
---
+++ tags/1.00.02/storefront.php
@@ -0,0 +1,51 @@
+<?php
+	/* Author: Bill Yu, Chun Wai Chan, Patrick Kowalski, Sahra Dilmaghanyan, Sami Jashromi
+	 * Date: Nov 17, 2011
+	 * File name: storefront.php
+	 * File description: Store front to show available merchandise.
+	 */
+
+	require_once ('common.php');
+	
+	validateSession() or die();	// Prevent the script from running further if this fails.
+	
+	/*
+	 * Function to list products available for purchase.
+	 * Return a list of product IDs mapping to product details.
+	 */
+	function getProducts() {
+		if (USE_DB) {
+			$succeed = true;
+			$msg = null;
+			$products = null;
+			
+			// read from product table
+			$q = "SELECT * FROM tbl_product";
+			if ($result = mysql_query($q)) {
+				while ($row = mysql_fetch_assoc($result)) {
+					$products[] = array("prod_id" => $row['prod_id'], "prod_name" => $row['prod_name'], "prod_desc" => $row['prod_desc'], "prod_image" => $row['prod_image']);
+				}
+			}
+			else {
+				$succeed = false;
+				$msg = mysql_error();
+			}
+			return array('succeed' => $succeed, 'msg' => $msg, 'products' => $products);
+		} else {
+			$resp = array("succeed" => true, "products" =>
+				array("prod_id" => 1, "prod_name" => "item1", "prod_desc" => "item1 description", "prod_image" => "./img/radioactive_blue.jpg"),
+				array("prod_id" => 2, "prod_name" => "item2", "prod_desc" => "item2 description", "prod_image" => "./img/dry_cow_dung.jpg"),
+				array("prod_id" => 3, "prod_name" => "item3", "prod_desc" => "item3 description", "prod_image" => "./img/bill.jpg")
+			);
+			return $resp;
+		}
+	}
+
+	/*
+	 * Script to invoke methods.
+	 */
+	$method = $_REQUEST['method'];
+	if ($method == 'getProducts') {
+		echo json_encode(getProducts());
+	}
+?>	
